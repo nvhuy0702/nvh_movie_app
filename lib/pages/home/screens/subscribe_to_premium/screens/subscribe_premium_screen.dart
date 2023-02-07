bd930d016e71f4ae3513d3d27e623f5129d69caf
VanHuy <nvhuy0702@gmail.com>
2023-02-07 17:10:45 +0700

    add subscribe premium screen

--- lib/pages/home/screens/subscribe_to_premium/screens/subscribe_premium_screen.dart
+++ lib/pages/home/screens/subscribe_to_premium/screens/subscribe_premium_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class SubscribePremiumScreen extends StatefulWidget {
   const SubscribePremiumScreen({Key? key}) : super(key: key);
 
@@ -21,9 +22,117 @@ class _SubscribePremiumScreenState extends State<SubscribePremiumScreen> {
         elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: Column(
-        children: [
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 48,
+            ),
+            const Text(
+              "Đăng ký Premium",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF2B3467),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const Text(
+              "Thỏa thích xem phim Full-HD\n mà không có hạn chế và không có quảng cáo",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            buildContainer(
+              9.99,
+              "tháng"
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            buildContainer(
+                99.99,
+                "năm"
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
+  Widget buildContainer(double? price, String? time) {
+    return Container(
+      height: 230,
+      width: 400,
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color(0xFF2B3467), width: 2),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/crown.png",
+            width: 70,
+            height: 70,
+          ),
+          RichText(
+            text: TextSpan(
+              text: '\$$price ',
+              style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
+              children: <TextSpan>[
+                TextSpan(
+                  text: '/$time',
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 20,
+                      color: Colors.black.withOpacity(0.6)),
+                ),
+              ],
+            ),
+          ),
+          const Divider(
+            thickness: 1,
+            endIndent: 16,
+            indent: 16,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          buildRow("Xem tất cả những gì bạn muốn. Quảng cáo miễn phí."),
+          buildRow("Cho phép phát trực tuyến 4K."),
+          buildRow("Chất lượng video và âm thanh tốt hơn."),
+        ],
+      ),
+    );
+  }
+
+  Widget buildRow(String content) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Icon(
+            Icons.check,
+            color: Colors.red,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Text(content)
         ],
       ),
     );
